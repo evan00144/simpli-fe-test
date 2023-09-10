ce9b535bbef4e5f4d10303a7b74d4939f99ce95a
evan00144 <evan2@student.umn.ac.id>
2023-09-10 14:37:52 +0700

    navbar mobile

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  collapsed:boolean=true;
 
+  collapse(){
+    this.collapsed=!this.collapsed
+  }
 }
